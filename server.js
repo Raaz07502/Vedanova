3e68f5eaf325eb06d9e067a081743db78181c073
Razz singh <rajkumarsingh@Rajs-MacBook-Air.local>
2026-07-07 17:57:19 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const dotenv=require ("dotenv") 
+const express=require("express")
+const mongoose=require("mongoose")
+const path=require("path")
+const PORT=6500
+const app=express()
+dotenv.config()
+
+// body parts 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+// set views folder in express
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+//public folder 
+app.use(express.static(path.join(__dirname,"public")))
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("Database connected successfully");
+})
+.catch((err) => {
+    console.log("Database error:", err);
+});
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+// service routes
+app.get("/service", (req, res) => {
+   res.render("service");
+});
+
+// contact routes
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
